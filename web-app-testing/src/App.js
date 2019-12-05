056f6968c2ebd9144f02cd2d347f1d37f2e0b19a
trodriguez89 <trodriguez89@gmail.com>
2019-12-05 10:05:03 -0800

    mvp wip

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,19 +1,52 @@
 import React, {useState} from 'react';
 import Scoreboard from "./components/Scoreboard";
 import Buttons from "./components/Buttons";
+import styled from "styled-components";
 
 
 import './App.css';
 
 function App() {
   const [strikes, setStrikes] = useState(0);
-  const [balls, setballs] = useState(0);
+  const [balls, setBalls] = useState(0);
+
+  const strike = () => {
+    if (strikes === 2 ) {
+      setStrikes(0)
+      setBalls(0)
+    } else {
+      setStrikes(strikes + 1)
+    }
+  }
+
+  const ball = () => {
+    if (balls === 3) {
+      setStrikes(0)
+      setBalls(0)
+    } else {
+      setBalls(balls + 1)
+    }
+  }
+
+  const foul = () => {
+    if (strikes === 2) {
+      setStrikes(2)
+    } else {
+      setStrikes(strikes + 1)
+    }
+  }
+
+  const hit = () => {
+    setStrikes(0);
+    setBalls(0)
+  }
+
 
   return (
-    <div className="App">
+    <div>
       <h2>Baseball!</h2>
       <Scoreboard strikes={strikes} balls={balls}/>
-      <Buttons strikes={strikes} balls={balls}/>
+      <Buttons strikes={strikes} balls={balls} strike={strike} ball={ball} foul={foul} hit={hit}/>
     </div>
   );
 }
--- web-app-testing/src/components/Buttons.js
+++ web-app-testing/src/components/Buttons.js
@@ -3,9 +3,10 @@ import React from "react";
 const Buttons = (props) => {
     return (
         <div>
-            <button>Strike!</button>
-            <button>Ball!</button>
-            <button>Foul!</button>
+            <button onClick={props.strike}>Strike!</button>
+            <button onClick={props.ball}>Ball!</button>
+            <button onClick={props.foul}>Foul!</button>
+            <button onClick={props.hit}>Hit!</button>
         </div>
     );
 }
